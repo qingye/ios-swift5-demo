7b04d322ea63703bdf9ea8bc58032849cfd82633
chris.yang <chris.yang@weimob.com>
2021-03-15 15:44:50 +0800

    双 window 广告页 & 切换

--- JdApp/JdApp/AdvertiseViewController.swift
+++ JdApp/JdApp/AdvertiseViewController.swift
@@ -12,7 +12,7 @@ class AdvertiseViewController: BaseViewController {
     // 延迟初始化 timer
     lazy var timer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue.global())
     // 倒计时的时间
-    var seconds = 2
+    var seconds = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,28 +37,63 @@ class AdvertiseViewController: BaseViewController {
     
     func terminer() {
         timer.cancel()
-        switchRootController()
+//        switchRootController()
+        switchWindow()
     }
     
-    //
-    // 一个 window 的情况：只用切换 rootViewController 就行
-    //
-    func switchRootController() {
-        let window = UIApplication.shared.windows.first!
+    // 同样两种方式可以实现：广告页 -> 主页面：
+    // 1. 两个 window 来分别控制不同的业务，然后基于过渡动画来切换 window；
+    // 2. 一个 window，两个 vc，分别是 主vc 和 广告vc，通过修改 window.rootViewController 来完成；
+    func switchWindow() {
+        // 第2个 window（广告窗口）
+        let window = UIApplication.shared.windows.last!
         
         // 过渡动画：淡出
         UIView.transition(with: window,
                           duration: 0.5,
                           options: .transitionCrossDissolve,
                           animations: {
-                            
+
+                            // 临时保存 UIView 是否开启动画的状态（默认是开启）
+                            // 之所以先禁止，是防止存在其它动画影响了当前动画
+                            // ----------------------------------------
+                            // 设置了禁止动画后：
+                            // 1. 当前所有正在执行动画的没有任何影响
+                            // 2. 未执行的将不会执行动画
+                            // ----------------------------------------
+                            //
+                            // 因为我们这个回调是动画已经开始，所以，并不会被强制停止,
+                            // 最后再恢复 UIView 是否开启动画的状态即可
                             let old = UIView.areAnimationsEnabled
                             UIView.setAnimationsEnabled(false)
-                            window.rootViewController = MainTabBarController()
+                            window.alpha = 0
                             UIView.setAnimationsEnabled(old)
 
                           }, completion: { _ in
-                            // Do Nothing
+                            // 切换到主 window，即我们的 MainTabBarController
+                            UIApplication.shared.windows.first?.makeKeyAndVisible()
                           })
     }
+    
+    //
+    // 一个 window 的情况：只用切换 rootViewController 就行
+    //
+//    func switchRootController() {
+//        let window = UIApplication.shared.windows.first!
+//
+//        // 过渡动画：淡出
+//        UIView.transition(with: window,
+//                          duration: 0.5,
+//                          options: .transitionCrossDissolve,
+//                          animations: {
+//
+//                            let old = UIView.areAnimationsEnabled
+//                            UIView.setAnimationsEnabled(false)
+//                            window.rootViewController = MainTabBarController()
+//                            UIView.setAnimationsEnabled(old)
+//
+//                          }, completion: { _ in
+//                            // Do Nothing
+//                          })
+//    }
 }
--- JdApp/JdApp/AppDelegate.swift
+++ JdApp/JdApp/AppDelegate.swift
@@ -10,17 +10,43 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+//    var window: UIWindow?
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.backgroundColor = .white
+//        window?.rootViewController = AdvertiseViewController() // 修改这里
+//        window?.makeKeyAndVisible()
+//
+//        return true
+//    }
+    
+    // 多个 window：
+    // 第 1 个用于主app；
+    // 第 2 个用于显示广告页；
+    var windows: [UIWindow]?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .white
-        window?.rootViewController = AdvertiseViewController() // 修改这里
-        window?.makeKeyAndVisible()
+        // 后一个 window 盖在前一个之上，可以通过：
+        // windows?[下标].makeKeyAndVisible() 来切换显示
+        windows = [
+            addWindowWithVC(MainTabBarController()),
+            addWindowWithVC(AdvertiseViewController())
+        ]
         
         return true
+    }
+
+    func addWindowWithVC(_ vc: UIViewController) -> UIWindow {
+        let window = UIWindow.init(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        return window
     }
 }
 
