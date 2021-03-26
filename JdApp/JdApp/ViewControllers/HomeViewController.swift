50c4565f806145ef117683e416ac6bf15afb29a1
chris.yang <chris.yang@weimob.com>
2021-03-26 18:19:41 +0800

    HomeVC refer BannerView

--- JdApp/JdApp/ViewControllers/HomeViewController.swift
+++ JdApp/JdApp/ViewControllers/HomeViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import BannerView
 
 class HomeViewController: BaseViewController {
+    
+    var bannerView: BannerView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +29,8 @@ class HomeViewController: BaseViewController {
         
         view.addSubview(label)
         
+        addBannerView()
+        
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
@@ -35,5 +40,15 @@ class HomeViewController: BaseViewController {
     @objc func tap() {
         // push 到下一级页面
         navigationController?.pushViewController(ViewController(), animated: true)
+    }
+    
+    func addBannerView() {
+        bannerView = BannerView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 200))
+        bannerView?.setData([
+            "https://cdn-1.blushmark.com/blushmark/upimg/c1/ba/8c0793797057964bfa65a210f76a43aafbd1c1ba.gif",
+            "https://cdn-1.blushmark.com/blushmark/upimg/d8/bd/3269b7e78582be3d7d17c194cc32fe8b4d48d8bd.jpg",
+            "https://cdn-1.blushmark.com/blushmark/upimg/bd/f8/8c836e32bced64eef2a8d916d50081a38819bdf8.jpg"
+        ], true)
+        view.addSubview(bannerView!)
     }
 }
